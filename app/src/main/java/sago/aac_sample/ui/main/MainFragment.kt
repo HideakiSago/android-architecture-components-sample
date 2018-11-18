7d8d537886fccbd15c70e7c270e9a7d4f61b627b
hideaki.sago <hideaki.sago@gmail.com>
2018-11-18 10:58:09 +0900

    EditTextの値を反映させるようにしてみた。

--- app/src/main/java/sago/aac_sample/ui/main/MainFragment.kt
+++ app/src/main/java/sago/aac_sample/ui/main/MainFragment.kt
@@ -3,9 +3,12 @@ package sago.aac_sample.ui.main
 import android.arch.lifecycle.ViewModelProviders
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.main_fragment.*
 import sago.aac_sample.R
 
 class MainFragment : Fragment() {
@@ -26,7 +29,18 @@ class MainFragment : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
-        // TODO: Use the ViewModel
+
+        input.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {}
+            override fun afterTextChanged(s: Editable) {
+                viewModel.setValue(s.toString())
+            }
+        })
+
+        viewModel.getValue().observe({ lifecycle }) {
+            message.text = it
+        }
     }
 
 }
--- app/src/main/java/sago/aac_sample/ui/main/MainViewModel.kt
+++ app/src/main/java/sago/aac_sample/ui/main/MainViewModel.kt
@@ -1,7 +1,19 @@
 package sago.aac_sample.ui.main
 
+import android.arch.lifecycle.LiveData
+import android.arch.lifecycle.MutableLiveData
 import android.arch.lifecycle.ViewModel
 
+
 class MainViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+
+    private val valueLiveData: MutableLiveData<String> = MutableLiveData()
+
+    fun getValue(): LiveData<String> {
+        return valueLiveData
+    }
+
+    fun setValue(value: String) {
+        this.valueLiveData.value = value
+    }
 }
