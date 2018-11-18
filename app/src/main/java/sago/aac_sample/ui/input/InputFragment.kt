baf0628eba34fad0563c878fe5482505f6cc2f86
hideaki.sago <hideaki.sago@gmail.com>
2018-11-18 11:12:50 +0900

    add Input View and ViewModel

---
+++ app/src/main/java/sago/aac_sample/ui/input/InputFragment.kt
@@ -0,0 +1,32 @@
+package sago.aac_sample.ui.main
+
+import android.arch.lifecycle.ViewModelProviders
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import sago.aac_sample.R
+
+class InputFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = InputFragment()
+    }
+
+    private lateinit var viewModel: InputViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return inflater.inflate(R.layout.input_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProviders.of(this).get(InputViewModel::class.java)
+
+    }
+
+}
---
+++ app/src/main/java/sago/aac_sample/ui/input/InputViewModel.kt
@@ -0,0 +1,8 @@
+package sago.aac_sample.ui.main
+
+import android.arch.lifecycle.ViewModel
+
+
+class InputViewModel : ViewModel() {
+
+}
